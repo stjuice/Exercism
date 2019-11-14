fb155d37b8df7a7e75477066beadd8a037a56591
Victoriia Tryfonova <v.tryfonova@sana-commerce.com>
2019-11-14 16:22:16 +0200

    AddedArmstrongNumbers

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using Linq;
 using System.Collections;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        
-        return false;
+        int digit = (int)Math.Floor(Math.Log10(number) + 1);
+        int n = (int)Math.Pow(10, digit - 1);
+        var tmp = number;
+        int res = 0;
+
+        for (int i = digit; i > 0; i--)
+        {
+            var t = tmp / n;
+            res += (int)Math.Pow(t, digit);
+            tmp = number % n;
+            n /= 10;
+
+        }
+
+        return (int)res == number;
     }
 }
